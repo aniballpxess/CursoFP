b40786ad4122ff326fa0fe3c4da42213a5cf4705
aniballpxess <82780611+aniballpxess@users.noreply.github.com>
2024-01-09 00:00:01 +0100

    Actualizacion diaria

--- ASIGNATURAS/Prog/PRACTICAS/EjerciciosProg/src/Testeos.java
+++ ASIGNATURAS/Prog/PRACTICAS/EjerciciosProg/src/Testeos.java
@@ -1,6 +1,3 @@
-import java.util.Random;
-import java.util.Scanner;
-
 public class Testeos {
 
     public static void main(String[] args) {
@@ -8,24 +5,7 @@ public class Testeos {
 
         // Your code to be measured goes here
         //--------------------------------------------------------------------------------------------------------------
-        var rng = new Random();
-        var input = new Scanner(System.in);
-        int counter = 0;
 
-        System.out.print("¿Cuantas cifras quieres que tenga el número ganador (5, 6 ó 7)?: ");
-        int length = Integer.parseInt(input.nextLine());
-        int winner = (int) (rng.nextDouble() * Math.pow(10, length));
-        System.out.println("Número ganador: " + winner);
-
-        while (true) {
-            int lotteryTicket = (int) (rng.nextDouble() * Math.pow(10, length));
-            counter++;
-
-            if (lotteryTicket == winner) {
-                break;
-            }
-        }
-        System.out.println("Número de intentos: " + counter);
         //--------------------------------------------------------------------------------------------------------------
         // Your code to be measured ends here
 
